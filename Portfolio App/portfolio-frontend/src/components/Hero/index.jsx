375d9eb27c2f01a532a091f22377fcd18313bdbe
Carli <carli.venter@gmail.com>
2024-02-12 15:22:38 +0200

    Modify styles of homepage hero section

--- Portfolio App/portfolio-frontend/src/components/Hero/index.jsx
+++ Portfolio App/portfolio-frontend/src/components/Hero/index.jsx
@@ -14,30 +14,33 @@ export default function Hero({
   bgImageUrl,
 }) {
   return (
-    <Div
-      className="cs-hero cs-style1 cs-bg cs-fixed_bg cs-shape_wrap_1"
+    <Div className="cs-bg cs-fixed_bg"
       style={{ backgroundImage: `url(${bgImageUrl})` }}
     >
-      <Div className="cs-shape_1" />
-      <Div className="cs-shape_1" />
-      <Div className="cs-shape_1" />
-      <Div className="container">
-        <Div className="cs-hero_text">
-          <h1 className="cs-hero_title">{parse(title)}</h1>
-          <Div className="cs-hero_info">
-            <Div>
-              <Button btnLink={btnLink} btnText={btnText} />
-            </Div>
-            <Div>
-              <Div className="cs-hero_subtitle">{subtitle}</Div>
+      <Div
+        className="cs-hero cs-style1 cs-shape_wrap_1 cs-dim_bg"
+      >
+        <Div className="cs-shape_1" />
+        <Div className="cs-shape_1" />
+        <Div className="cs-shape_1" />
+        <Div className="container">
+          <Div className="cs-hero_text">
+            <h1 className="cs-hero_title">{parse(title)}</h1>
+            <Div className="cs-hero_info">
+              {/* <Div>
+                <Button btnLink={btnLink} btnText={btnText} />
+              </Div> */}
+              <Div>
+                <Div className="cs-hero_subtitle">{subtitle}</Div>
+              </Div>
             </Div>
           </Div>
         </Div>
+        {/* <VerticalLinks data={heroSocialLinks} title={socialLinksHeading} />
+        <a href={scrollDownId} className="cs-down_btn">
+          .
+        </a> */}
       </Div>
-      <VerticalLinks data={heroSocialLinks} title={socialLinksHeading} />
-      <a href={scrollDownId} className="cs-down_btn">
-        .
-      </a>
     </Div>
   );
 }
