efca9541781be1aa45b4c42cb256e5692c981828
Carli <carli.venter@gmail.com>
2024-03-05 20:30:20 +0200

    Correct font import bug

--- Portfolio App/portfolio-frontend/src/pages/_document.jsx
+++ Portfolio App/portfolio-frontend/src/pages/_document.jsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head> 
+        {/* Google Fonts */}
+        <link href="https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
